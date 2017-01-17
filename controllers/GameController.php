1d92dd8876483f5033d0a54cc97c7534047ceb3c
Osein <osein.wtr@gmail.com>
2017-01-17 20:17:11 +0200

    global update code

--- controllers/GameController.php
+++ controllers/GameController.php
@@ -14,12 +14,34 @@ class GameController extends BaseController
 
     public function initialize()
     {
+        global $config;
+
         if(!$this->session->get('user_id')) {
             return $this->response->redirect(getUrl(''));
         }
 
         $this->user = ORM::for_table('user')->find_one($this->session->get('user_id'));
         $this->view->user = $this->user;
+
+        $lastUpdate = $this->user->last_update;
+        $timeNow = time();
+        $timeDiff = $timeNow - $lastUpdate;
+
+        if($timeDiff > 0) {
+            if($this->user->ap_now < $this->user->ap_max) {
+                $apPerSecond = $config->apPerHour / 3600;
+                $apDelta = $apPerSecond * $timeDiff;
+                $this->user->ap_now = min($apDelta + $this->user->ap_now, $this->user->ap_max);
+            }
+
+            if($this->user->hp_now < $this->user->hp_max) {
+                $hpPerSecond = ($config->basicRegen + $config->endRegenRatio * $this->user->end) / 3600;
+                $hpDelta = $hpPerSecond * $timeDiff;
+                $this->user->hp_now = min($hpDelta + $this->user->hp_now, $this->user->hp_max);
+            }
+
+            $this->user->last_update = $timeNow;
+        }
     }
 
     public function afterExecuteRoute()
