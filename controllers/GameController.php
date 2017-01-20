5edbb7633e32541869784df2857e65cec4a6b1a4
Osein <huseyin.gucuyener@gmail.com>
2017-01-20 18:37:32 +0200

    search page

--- controllers/GameController.php
+++ controllers/GameController.php
@@ -58,7 +58,31 @@ class GameController extends BaseController
     }
 
     public function postSearch() {
+        $searchType = $this->request->get('searchtyp');
+        $search = $this->request->get('text');
+        $exact = $this->request->get('exakt');
+        $this->view->searchString = $search;
+        $this->view->searchType = $searchType;
+        $this->view->exact = $exact;
 
+        if($searchType == 'name') {
+            $this->view->search_type = 'player';
+            $result = ORM::for_table('user')
+                ->select_many('id', 'name', 'race', 's_booty');
+
+            if($exact) {
+                $result = $result->where('name', $search);
+            } else {
+                $result = $result->where_like('name', '%'.$search.'%');
+            }
+
+            $this->view->results = $result
+                ->limit(25)
+                ->find_many();
+        }
+
+        $this->view->menu_active = 'search';
+        $this->view->pick('user/search');
     }
 
 }
