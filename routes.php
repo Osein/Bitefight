b16d930d555fb8e057ecab93b368a614b0df7e8e
Osein <huseyin.gucuyener@gmail.com>
2017-01-21 03:20:50 +0200

    router edit

--- routes.php
+++ routes.php
@@ -12,13 +12,14 @@ $router->removeExtraSlashes(true);
 $router->setDefaultController('Home');
 $router->setDefaultAction('getIndex');
 
-$router->addGet('/news', ['controller' => 'Home', 'action' => 'getNews']);
-$router->addGet('/highscore', ['controller' => 'Home', 'action' => 'getHighscore']);
+$router->addGet('/news', ['controller' => 'Base', 'action' => 'getNews']);
+$router->addGet('/highscore', ['controller' => 'Base', 'action' => 'getHighscore']);
+$router->addGet('/profile/player/{id:[0-9]+}', ['controller' => 'Base', 'action' => 'getPreview']);
 
 $register = new \Phalcon\Mvc\Router\Group(['controller' => 'Home']);
 $register->setPrefix('/register');
-$register->addGet('/{id:[0-9]}', ['action' => 'getRegister']);
-$register->addPost('/{id:[0-9]}', ['action' => 'postRegister']);
+$register->addGet('/{id:[0-2]}', ['action' => 'getRegister']);
+$register->addPost('/{id:[1-2]}', ['action' => 'postRegister']);
 $register->addGet('/ajaxcheck', ['action' => 'postAjaxCheck']);
 $router->mount($register);
 
@@ -33,11 +34,12 @@ $user->addGet('/profile', ['action' => 'getProfile']);
 $user->addPost('/profile/training', ['action' => 'postTrainingUp']);
 $user->addGet('/profile/logo', ['action' => 'getProfileLogo']);
 $user->addPost('/profile/logo', ['action' => 'postProfileLogo']);
-$user->addGet('/news', ['action' => 'getNews']);
 $user->addGet('/settings', ['action' => 'getSettings']);
 $user->addPost('/settings', ['action' => 'postSettings']);
-$user->addGet('/highscore', ['action' => 'getHighscore']);
 $router->mount($user);
+
+$router->addGet('/hideout', ['controller' => 'User', 'action' => 'getHideout']);
+$router->addGet('/hideout/upgrade', ['controller' => 'User', 'action' => 'getHideoutUpgrade']);
 
 $hunt = new \Phalcon\Mvc\Router\Group(['controller' => 'Hunt']);
 $hunt->setPrefix('/hunt');
@@ -45,13 +47,6 @@ $hunt->addGet('/index', ['action' => 'getHunt']);
 $hunt->addGet('/human/{id:[1-5]}', ['action' => 'getHumanHunt']);
 $router->mount($hunt);
 
-$hideout = new \Phalcon\Mvc\Router\Group(['controller' => 'User']);
-$hideout->setPrefix('/hideout');
-$hideout->addGet('', ['action' => 'getHideout']);
-$hideout->addGet('/upgrade', ['action' => 'getHideoutUpgrade']);
-$router->mount($hideout);
-
-$router->addGet('/profile/player/{id:[0-9]+}', ['controller' => 'Base', 'action' => 'getPreview']);
 $router->addGet('/search', ['controller' => 'Game', 'action' => 'getSearch']);
 $router->addPost('/search', ['controller' => 'Game', 'action' => 'postSearch']);
 $router->addGet('/notepad', ['controller' => 'User', 'action' => 'getNotepad']);
