9fcccec514c96b047e7f3fc17df03ef5f71d9f2a
Osein <huseyin.gucuyener@gmail.com>
2017-04-18 22:30:01 +0300

    added info popup view, message controller

---
+++ controllers/MessageController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: osein
+ * Date: 18/04/17
+ * Time: 22:20
+ */
+
+namespace Bitefight\Controllers;
+
+class MessageController extends GameController
+{
+
+}
